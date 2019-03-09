4ebbdbd105d60e3d78d30d41e27a819f0deffae6
Maribel Montejano <outdoorsole@users.noreply.github.com>
2019-03-09 11:20:46 -0800

    Remove boilerplate components from app; outline detox tests (starting point)

---
+++ e2e/managingRestaurants.spec.js
@@ -0,0 +1,12 @@
+describe('Example', () => {
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
+  it('should allow creating a restaurant', async () => {
+    // click add restaurant button
+    // enter restaurant name
+    // click save button
+    // check to make sure restaurant appears in the list
+  });
+});
