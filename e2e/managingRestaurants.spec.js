d4f4696fa9c12b5ee06b8c97b8720fb658b65743
Maribel Montejano <outdoorsole@users.noreply.github.com>
2019-03-09 11:35:06 -0800

    Create tests to match actions (currently failing)

--- e2e/managingRestaurants.spec.js
+++ e2e/managingRestaurants.spec.js
@@ -5,8 +5,16 @@ describe('Example', () => {
 
   it('should allow creating a restaurant', async () => {
     // click add restaurant button
+    await element(by.id('newRestaurantButton')).tap();
+
     // enter restaurant name
+    await element(by.id('restaurantNameTextField')).typeText('Sushi Place');
+
     // click save button
+    await element(by.id('saveRestaurantButton')).tap();
+
     // check to make sure restaurant appears in the list
+    await expect(element(by.label('Sushi Place'))).toBeVisible();
+    await expect(element(by.id('restaurantNameTextField'))).toBeNotVisible();
   });
 });
